db21af325565f29197a4301ed181be0ac2f8c083
aldm <d44.a1@yandex.ru>
2023-10-20 22:33:13 +0300

    2.0

--- backend/app.js
+++ backend/app.js
@@ -16,7 +16,7 @@ const { PORT = 3000, DB_URL = 'mongodb://127.0.0.1:27017/mestodb' } = process.en
 mongoose.connect(DB_URL, {
   useNewUrlParser: true,
 }).then(() => {
-  console.log('Подключен к БД');
+  // console.log('Подключен к БД');
 });
 
 app.use(helmet());
@@ -37,5 +37,5 @@ app.use(errors());
 app.use(handleError);
 
 app.listen(PORT, () => {
-  console.log(`Приложение запущено, порт: ${PORT}`);
+  // console.log(`Приложение запущено, порт: ${PORT}`);
 });
